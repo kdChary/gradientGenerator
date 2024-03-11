fc710f2c05e619ba3d01d1bd4dc3a583719bae87
Siva Dinesh k <ksd.ccbp4@gmail.com>
2024-03-11 08:31:40 +0000

    gradientItem StyledComponent added

--- src/components/GradientDirectionItem/styledComponents.js
+++ src/components/GradientDirectionItem/styledComponents.js
@@ -1,1 +1,18 @@
-// Style your elements here
+import styled from 'styled-components'
+
+export const DirectionItem = styled.ul`
+  list-style-type: none;
+  border: none;
+  background-color: transparent;
+`
+
+export const DirectionButton = styled.button`
+  width: 48px;
+  height: 27px;
+  font-family: 'Roboto';
+  font-weight: 600;
+  color: #334155;
+  border: solid 1px #ededed50;
+  border-radius: 4px;
+  background-color: ${props => (props.active ? '#ededed50' : '#ededed')};
+`
